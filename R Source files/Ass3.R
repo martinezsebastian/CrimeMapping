3b35adb06e654976a7cf4c177e77cff065dabfc2
martinezsebastian <sebastian.martinez32@gmail.com>
2015-11-12 18:08:44 +0100

    Advancement on distances

--- R Source files/Ass3.R
+++ R Source files/Ass3.R
@@ -1,5 +1,5 @@
 library(foreign)
-setwd("/Users/SebastianMartinez/Dropbox/0. Hertie/3/Collaborative Social Science Data Analysis/5/")
+setwd("/Users/SebastianMartinez/Dropbox/0. Hertie/3/Collaborative Social Science Data Analysis/GitHub/CrimeMapping/R Source files/")
 crimes <- read.csv("crimes_2015.csv")
 View(crimes)
 
@@ -11,8 +11,32 @@ total$y_dist <- total$ycoordinate - total$Y
 total$x_dist <- total$xcoordinate - total$X
 total$distance <- sqrt((total$x_dist)^2+(total$y_dist)^2)
 total$count <- 1
+
+community_centroids <- read.csv("Comm_centroids.csv")
+View(community_centroids)
+total <- merge(total, community_centroids, by.x = "communityarea", by.y = "AREA_NUMBE")
+
+
+counter <- aggregate(count ~ district, data = total, FUN=mean)
+
+for(i in counter$district) 
+{
+  temp <- total[total[,2]!=i,]
+  nam <- paste("District", i, sep = "")
+  assign(nam, temp)
+}
+
+
+
 aggtotal <- aggregate(distance ~ district*primarytype, data=total, FUN=mean)
 aggtotal$number <- aggregate(count ~ district*primarytype, data=total, FUN=sum)
+
+
+
+
+
+
+
 
 library(maptools)
 library(ggmap)
