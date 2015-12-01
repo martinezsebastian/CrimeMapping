131b2d0026f12c1ae3bd86137c0cab8b8bbcb4a7
martinezsebastian <sebastian.martinez32@gmail.com>
2015-12-01 17:17:13 +0100

    update!

--- RSourceFiles/Presentation.R
+++ RSourceFiles/Presentation.R
@@ -197,15 +197,26 @@ summary(violent_model1)
 ##################################
 
 ## ZELIG
-Crime_Data$hardship <- as.integer(Crime_Data$hardship_index)
-Crime_Data$close_ <- as.factor(Crime_Data$close)
-Z1 <- zelig(close_ ~ hardship + violent, cite = FALSE, data = Crime_Data, model = 'logit')
+##defining variables 
+PerCapitaIncome <- as.integer(Crime_Data$per_capita_income_)
+Over16Unemployed <- as.integer(Crime_Data$percent_aged_16_unemployed)
+CrimeIsViolent <- as.factor(Crime_Data$violent)
+CrimeIsClose <- Crime_Data$close
+
+x <- list(PerCapitaIncome, Over16Unemployed, CrimeIsViolent, CrimeIsClose)
+CrimeData <- as.data.frame(x)
+
+library(Zelig)
+Z1 <- zelig(CrimeIsClose ~ PerCapitaIncome + Over16Unemployed + CrimeIsViolent, cite = FALSE, data = CrimeData, model = 'logit')
 summary(Z1)
-setZ1 <- setx(Z1, hardship = 1:2)
-simZ1 <- sim(Z1, x = setZ1)
+
+# setting fitted values
+setZ1 <- setx(Z1, Over16Unemployed = 4:22)
+
+##Run simulation 
+simZ1 <- sim(Z1, x = setZ1, num = 500)
 plot(simZ1)
 
-write.csv(Crime_Data$hardship, "hardship.csv")
 
 
 
